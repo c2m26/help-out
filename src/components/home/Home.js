18e9fbdfdb86fa6371ac7a50497647943be02aab
c2m26 <carlos@c2m-webdev.com>
2020-01-06 15:55:01 +0100

    first commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Registration from './auth/Registration.js'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        Teste Inicial
+        <Registration />
+      </div>
+    )
+  }
+
+}
+
+export default Home
